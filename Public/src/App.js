ea23a13ed7ee8dfdbf7389ba2f1427901614fdaf
Ashish Kumar <ashish4538@gmail.com>
2022-11-27 01:04:58 +0530

    first commit

---
+++ Public/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import Chat from "./Pages/Chat";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
+export default function App(){
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/" element={<Chat/>} />
+    </Routes>
+      </BrowserRouter>
+  );
+}
